e0610a9e86a10e32b98b115a4f5d00060af78bf3
case02 <cstocker0212@gmail.com>
2022-12-19 14:36:20 -0800

    added method-override middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 // dependencies
 const express = require('express')
 const app = express()
-// const app = require('express').express()
 const cors = require('cors')
+const methodOverride = require('method-override');
 require('./models')
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -17,7 +17,8 @@ app.use(cors())
 // parse the body data
 app.use(express.urlencoded({extended: true }))
 app.use(express.json())
-
+// method-override allows us to interpret POST requests from the browser as another request type: DELETE, PUT, etc.
+app.use(methodOverride("_method"));
 //routes
 app.use('/user', userCtrl)
 app.use('/comment', commentCtrl)
