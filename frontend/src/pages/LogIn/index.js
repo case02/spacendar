e7670da3fab6069d5e68816025d2831595bdd9ec
gititgirl <jordanlind2017@gmail.com>
2022-12-20 17:08:32 -0800

    created frontend login index

---
+++ frontend/src/pages/LogIn/index.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+export default function LogIn(props) {
+    const [formState, setFormState] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (event) => {
+        setFormState({...formState, [event.target.name]: event.target.value })
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
+    return (
+        <div className="form">
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username: </label>
+                <input
+                    id="username"
+                    type="text"
+                    onChange={handleChange}
+                    value={formState.username}
+                />
+                <label htmlFor="password">Password: </label>
+                <input
+                    id="password"
+                    type="password"
+                    onChange={handleChange}
+                    value={formState.password}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
