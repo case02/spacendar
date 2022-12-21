7ae18b373c9e4419f1f9c61684e26b19816ba901
gititgirl <jordanlind2017@gmail.com>
2022-12-21 10:56:43 -0800

    nav and footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,11 +3,13 @@ export default function Footer() {
     return (
 			<footer>
 				<img
-					alt='logo'
+					alt='Spacendar'
 					className='logo'
 					src='frontend/src/assets/images/spacendar.png'
 					width='200px'
 				/>
+				<h4>Contributors: </h4>
+				<h5>Lianne Aratea, Pierce Goldbeck, Jordan Lind, and Casey Stocker</h5>
 			</footer>
 		);
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+
 export default function SignUp() {
     const initialState = { username: '', password: '' };
 	const [formState, setFormState] = useState(initialState);
