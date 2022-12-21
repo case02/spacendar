ca231bbf133b3c2e91a23799b22a1b5244fbecc9
lbaratea <aratealianne@gmail.com>
2022-12-20 17:13:25 -0800

    added functionality to the sign up form

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,17 +1,34 @@
 export default function SignUp() {
+    const initialState = { username: '', password: '' };
+	const [formState, setFormState] = useState(initialState);
+
+    const handleChange = (event) => {
+        setFormState({ ...formState, [event.target.id]: event.target.value });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormState(initialState);
+    }
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<label>
 				<p>Username</p>
-				<input type='text' name='username' />
+				<input 
+                type='text' 
+                name='username'
+                value={formState.username}
+                onChange={handleChange} />
 			</label>
 			<label>
 				<p>Password</p>
-				<input type='password' name='password' />
+				<input 
+                type='password' 
+                name='password' 
+                value={formState.password}
+                onChange={handleChange}/>
 			</label>
 			<div>
 				<button typ='submit'>Sign Up</button>
 			</div>
 		</form>
-	);
-}
+	)}};
