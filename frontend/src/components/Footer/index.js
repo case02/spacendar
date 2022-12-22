7bd545ba6d938983f28b8193fb9e8be17ac13826
lbaratea <aratealianne@gmail.com>
2022-12-22 15:39:22 -0800

    added logo as home button plus hover transitions

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,16 +1,10 @@
-import './styles.css'
+import './styles.css';
 export default function Footer() {
-    return (
-			<footer>
-				<img
-					alt='Spacendar'
-					className='logo'
-					src='frontend/src/assets/images/spacendar.png'
-					width='200px'
-				/>
-				<h4>Contributors: </h4>
-				<h5>Lianne Aratea, Pierce Goldbeck, Jordan Lind, and Casey Stocker</h5>
-				
-			</footer>
-		);
+	return (
+		<footer>
+			<a className="link" href='https://apod.nasa.gov/apod/calendar/allyears.html'>NASA APOD</a>
+			<h4>Contributors: </h4>
+			<h5>Lianne Aratea, Pierce Goldbeck, Jordan Lind, and Casey Stocker</h5>
+		</footer>
+	);
 }
--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -3,51 +3,61 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import spacendar from '../../assets/images/spacendar.png';
 
 // styles
-import './styles.css'
+import './styles.css';
 
 export default function Nav(props) {
-    // state declaration: build JSX array of NavBar items
-    const initialState = [
-			<div className='nav-item' key='1'>
-				<Link to='/month'>
-                    <h1>Month</h1>
-                </Link>
-			</div>,
+	// state declaration: build JSX array of NavBar items
+	const initialState = [
+		<div className='nav-item' key='1'>
+			<Link to='/month'>
+				<h1>Month</h1>
+			</Link>
+		</div>,
 
-			<div className='nav-item' key='2'>
-				<Link to='/'>
-					<h1>Home</h1>
-				</Link>
-			</div>
-		]
+		<div className='nav-item' key='2'>
+			<Link to='/'>
+				<img alt='Spacendar' className='logo' src={spacendar} width='400px' />
+			</Link>
+		</div>,
+	];
 
-        
-    const [navItems, setNavItems] = useState(initialState)
+	const [navItems, setNavItems] = useState(initialState);
 
-    // add NavBar items to JSX array depending on App login state
-    useEffect(() => {
-        if (props.isLoggedIn) {
-            setNavItems(initialState.concat(
-                <div className="nav-item" key='3'>
-                    <Button class="btn btn-dark btn-lg" onClick={() => { props.setLogInStatus(false) }}>Log Out</Button>
-                </div>
-            ))
-        } else {
-            setNavItems(initialState.concat([
-                <div className="nav-item" key='3'>
-                    <Link to= '/user/signup'>Sign Up</Link>
-                    <Link to= '/user/login'>Log In</Link>
-                </div>
-            ]))
-        }
-    }, [props.isLoggedIn])
+	// add NavBar items to JSX array depending on App login state
+	useEffect(() => {
+		if (props.isLoggedIn) {
+			setNavItems(
+				initialState.concat(
+					<div className='nav-item' key='3'>
+						<Button
+							class='btn btn-outline-light btn-lg'
+							onClick={() => {
+								props.setLogInStatus(false);
+							}}>
+							Log Out
+						</Button>
+					</div>
+				)
+			);
+		} else {
+			setNavItems(
+				initialState.concat([
+					<div className='nav-item' key='3'>
+						<Link to='/user/signup'>
+							<Button class='btn btn-outline-light btn-lg'>Sign Up</Button>
+						</Link>
+						<Link to='/user/login'>
+							<Button class='btn btn-outline-light btn-lg'>Log In</Button>
+						</Link>
+					</div>,
+				])
+			);
+		}
+	}, [props.isLoggedIn]);
 
-    // render JSX
-    return (
-        <nav>
-            {navItems}
-        </nav>
-    )
+	// render JSX
+	return <nav>{navItems}</nav>;
 }
