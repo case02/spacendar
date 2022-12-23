a86ffeb9d2069f80fdafc4f920a062c7989e84b0
case02 <cstocker0212@gmail.com>
2022-12-23 09:49:47 -0800

    added user btn to footer

--- frontend/src/pages/Month/index.js
+++ frontend/src/pages/Month/index.js
@@ -1,5 +1,6 @@
 
 import './month.css';
+import './styles.css'
 
 
 export default function Month(props) {
@@ -7,7 +8,7 @@ export default function Month(props) {
         <div className='month-container'>
             <h1>December</h1>
 
-            <div className="wk1">
+            {/* <div className="wk1">
                 {props.monthImages.slice(0,7).map((a, i) => {
                     return (
                         <div class="test">
@@ -25,14 +26,16 @@ export default function Month(props) {
                         </div>
                     )
                 })}
+            </div> */}
+
+            <div className="month-cal">
+                {props.monthImages.map((a, i) => (
+                    <div className='day'>
+                        <p>{i +1}</p>
+                        <img alt='day-pic' src={props.monthImages[i].hdurl} width='200px' />
+                    </div>
+                ))}
             </div>
-
-
-            {/* // {props.monthImages.map((a, i) => (
-            //     <div className='day'>
-            //         <p>{i +1}</p>
-            //         <img alt='day-pic' src={props.monthImages[i].hdurl} width='200px' />
-            //     </div>
-            // ))} */}
+            
         </div> 
 		)}
