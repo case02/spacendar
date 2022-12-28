eb6185e97efadf49663a8109f8bf9260d0a7722c
gititgirl <jordanlind2017@gmail.com>
2022-12-28 10:55:47 -0800

    month styling

--- frontend/src/pages/Month/index.js
+++ frontend/src/pages/Month/index.js
@@ -7,43 +7,32 @@ import Comment from '../../components/Comment';
 export default function Month(props) {
 	return (
 		<div className='month-container'>
-			<div className='month-cal'>
 				<h1>December</h1>
-				<div className='days-of-week'>
-					<div>Sunday</div>
-					<div>Monday</div>
-					<div>Tuesday</div>
-					<div>Wednesday</div>
-					<div>Thursday</div>
-					<div>Friday</div>
-					<div>Saturday</div>
-				</div>
-				<div className='blank-day'></div>
-				<div className='blank-day'></div>
-				<div className='blank-day'></div>
-				<div className='blank-day'></div>
-				{props.monthImages.map((day, i) => (
-					<Link key={i} to={`/day/${props.monthImages[i].date}`}>
-						<div className='d-flex flex-wrap align-content-xl-stretch'>
-							<div className='date-number'>
-								<p>{i + 1}</p>
+				<div className='month-cal'>
+					<div className='day-map'>
+						{props.monthImages.map((day, i) => (
+						<Link key={i} to={`/day/${props.monthImages[i].date}`}>
+							<div className='d-flex flex-wrap align-content-xl-stretch'>
+								<div className='date-number'>
+									<p>{i + 1}</p>
+								</div>
+
+								{props.monthImages[i].media_type === 'image' && (
+									<img
+										alt='day-pic'
+										src={props.monthImages[i].hdurl}
+										width='230px'
+									/>
+								)}
+
+								{props.monthImages[i].media_type === 'video' && (
+									<img alt='day-vid' src={wormhole} width='230px' />
+								)}
 							</div>
-
-							{props.monthImages[i].media_type === 'image' && (
-								<img
-									alt='day-pic'
-									src={props.monthImages[i].hdurl}
-									width='230px'
-								/>
-							)}
-
-							{props.monthImages[i].media_type === 'video' && (
-								<img alt='day-vid' src={wormhole} width='230px' />
-							)}
-						</div>
-					</Link>
-				))}
-			</div>
+						</Link>
+					))}
+					</div>
+					</div>
 			<div className='comment-section'>
 				<Comment />
 			</div>
